0d7c0b2de8cc56b6ba00b0cc1f386c3322167013
mohammad <mohammadghiasi005@gmail.com>
2024-03-30 21:46:20 +0330

    key-prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Box className="grid gap-y-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" >
       {shoose.map((shooe: ShooseItem): JSX.Element => {
-        return <Product id={shooe.id} image={shooe.image} name={shooe.name} price={shooe.price} />;
+        return <Product key={shooe.id} id={shooe.id} image={shooe.image} name={shooe.name} price={shooe.price} />;
       })}
     </Box>
   );
